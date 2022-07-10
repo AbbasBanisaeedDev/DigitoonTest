16b63f2d7a0c4e74d0592adecc67d3321e636089
Abbas banisaeed <abbas.bensaeed68@gmail.com>
2022-07-11 00:27:53 +0430

    AddDigitoonTest

--- app/src/main/java/github/abbasbanisaeed/digitoon_test/utils/Constants.kt
+++ app/src/main/java/github/abbasbanisaeed/digitoon_test/utils/Constants.kt
@@ -5,5 +5,5 @@ import github.abbasbanisaeed.digitoon_test.BuildConfig
 object Constants {
     const val BASE_URL = "https://www.omdbapi.com/"
     const val CONNECTION_TIME = 60L
-    const val API_KEY = BuildConfig.API_KEY
+    const val API_KEY ="3e974fca"
 }
